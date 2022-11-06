ff51b3362564d1a35a4e5e4f81631c4dd2a6c986
zSnails <erizojuan33@gmail.com>
2022-11-05 19:52:56 -0600

    xd

--- src/main/java/com/zsnails/poo/App.java
+++ src/main/java/com/zsnails/poo/App.java
@@ -1,24 +1,11 @@
 package com.zsnails.poo;
 
-import javax.swing.JFrame;
-
-import com.zsnails.hiddenNumber.HiddenNumberGame;
-
-import com.zsnails.player.Player;
-import com.zsnails.hiddenNumber.HiddenNumberGameUI;
+import javax.swing.JButton;
 
 public class App {
+    JButton[] juegos = {};
+
     public static void main(String[] args) {
-        JFrame wHandle = new JFrame("Nigger dog");
-        wHandle.setSize(500, 500);
-
-        Player e = new Player("Samuel", "");
-
-
-        HiddenNumberGameUI i = new HiddenNumberGameUI();
-        i.iniciarPartida(e);
-
-
-        // wHandle.setVisible(true);
+        
     }
 }
